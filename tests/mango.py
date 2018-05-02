de9e2ddffbfa025f0c81621a6a53a8e27a9e2170
pingze <wang719695@gmail.com>
2018-05-02 11:30:48 +0800

    原生方法改为动态创建；增加大部分原生方法

--- tests/mango.py
+++ tests/mango.py
@@ -66,16 +66,82 @@ class ListField(Field):
 class DictField(Field):
     _type = dict
 
+# ****** Model Funcs ******
+
+def cls_method_creater(func_name):
+    '直接生成pymongo原生方法的包装类方法'
+    @classmethod
+    def func(cls, *args, **kargs):
+        # args[0]为插入字段时，做替换，清除未声明的field
+        if func_name in ['insert', 'create']:
+            args = (cls.filte_field(args[0]),) + args[1:]
+        else:
+            cls.filte_field(args[0])
+        func_real = getattr(db[cls.Meta.collection], func_name)
+        returned = func_real(*args, **kargs)
+        return returned
+    return func
+
+@classmethod
+def find(cls, *args, **kargs):
+    cursor = db[cls.Meta.collection].find(*args, **kargs)
+    # 可以实现直接返回Model对象，但这样返回字典手动转换，更灵活
+    return cursor
+
+@classmethod
+def find_one(cls, *args, **kargs):
+    data_dict = db[cls.Meta.collection].find_one(*args, **kargs)
+    return cls.dict2obj(data_dict)
+
+@classmethod
+def insert(cls, *args, **kargs):
+    args = (cls.filte_field(args[0]),) + args[1:]
+    _id = db[cls.Meta.collection].insert(*args, **kargs)
+    return _id
+
+@classmethod
+def create(cls, **kargs):
+    kargs = cls.filte_field(kargs)
+    _id = db[cls.Meta.collection].insert(kargs)
+    return _id
+
+@classmethod
+def update(cls, *args, **kargs):
+    # cls.filte_field(args[0])
+    # TODO pymongo的update使用多重字典，无法直接做检查
+    returned = db[cls.Meta.collection].update(*args, **kargs)
+    return returned
+
+@classmethod
+def remove(cls, *args, **kargs):
+    # cls.filte_field(args[0])
+    # TODO pymongo的remove使用多重字典，无法直接做检查
+    returned = db[cls.Meta.collection].remove(*args, **kargs)
+    return returned
+
 
 # ****** Model ******
 
 class Model(object):
-    find = None
-    find_one = None
-    insert = None
-    create = None
-    update = None
-    remove = None
+    find = find
+    find_one = find_one
+    insert = cls_method_creater('insert')
+    create = create
+    update = cls_method_creater('update')
+    remove = cls_method_creater('remove')
+    insert_many = cls_method_creater('insert_many')
+    insert_one = cls_method_creater('insert_one')
+    index_information = cls_method_creater('index_information')
+    update_many = cls_method_creater('update_many')
+    ensure_index = cls_method_creater('ensure_index')
+    drop_indexes = cls_method_creater('drop_indexes')
+    drop_index = cls_method_creater('drop_index')
+    delete_one = cls_method_creater('delete_one')
+    delete_many = cls_method_creater('delete_many')
+    create_indexes = cls_method_creater('create_indexes')
+    create_index = cls_method_creater('create_index')
+    count = cls_method_creater('count')
+    aggregate = cls_method_creater('aggregate')
 
     class Meta():
         strict = False
@@ -87,6 +153,7 @@ class Model(object):
     def __init_subclass__(subcls, **kargs):
         subcls.Meta.collection = subcls.Meta.collection if hasattr(subcls.Meta, 'collection') else subcls.__name__.lower()
         collection = subcls.Meta.database[subcls.Meta.collection]
+        print(dir(collection))
         if hasattr(subcls.Meta, 'index'):
             for index in subcls.Meta.index:
                 try:
@@ -136,52 +203,12 @@ class Model(object):
 
     @classmethod
     def filte_field(cls, field_dict):
+        new_dict = {}
         for key in field_dict:
             val = field_dict[key]
             if not hasattr(cls, key) or not isinstance(getattr(cls, key), Field):
                 continue
             getattr(cls, key).field_assert(val, key)
+            new_dict[key] = val
+        return new_dict
 
-@classmethod
-def find(cls, *args, **kargs):
-    cursor = db[cls.Meta.collection].find(*args, **kargs)
-    # 可以实现直接返回Model对象，但这样返回字典手动转换，更灵活
-    return cursor
-
-@classmethod
-def find_one(cls, *args, **kargs):
-    data_dict = db[cls.Meta.collection].find_one(*args, **kargs)
-    return cls.dict2obj(data_dict)
-
-@classmethod
-def insert(cls, *args, **kargs):
-    cls.filte_field(args[0])
-    _id = db[cls.Meta.collection].insert(*args, **kargs)
-    return _id
-
-@classmethod
-def create(cls, **kargs):
-    cls.filte_field(kargs)
-    _id = db[cls.Meta.collection].insert(kargs)
-    return _id
-
-@classmethod
-def update(cls, *args, **kargs):
-    # cls.filte_field(args[0])
-    # TODO pymongo的update使用多重字典，无法直接做检查
-    returned = db[cls.Meta.collection].update(*args, **kargs)
-    return returned
-
-@classmethod
-def remove(cls, *args, **kargs):
-    # cls.filte_field(args[0])
-    # TODO pymongo的remove使用多重字典，无法直接做检查
-    returned = db[cls.Meta.collection].remove(*args, **kargs)
-    return returned
-
-setattr(Model, 'find', find)
-setattr(Model, 'find_one', find_one)
-setattr(Model, 'insert', insert)
-setattr(Model, 'create', create)
-setattr(Model, 'update', update)
-setattr(Model, 'remove', remove)
